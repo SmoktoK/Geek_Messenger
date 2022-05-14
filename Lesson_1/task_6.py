7c1043df88c04253a6aacaf492a5659c133afb65
SmoktoK <smokotoff@yandex.ru>
2022-05-14 21:36:59 +0300

    Add requirements.txt

--- Lesson_1/task_6.py
+++ Lesson_1/task_6.py
@@ -1,13 +1,13 @@
 # Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет», «декоратор».
 # Проверить кодировку файла по умолчанию. Принудительно открыть файл в формате Unicode и вывести его содержимое.
+import chardet
 
-with open('test_file.txt') as file:
-    print(f'Кодировка файла: {file.encoding}')
-    file.close()
+with open('test_file.txt', 'rb') as file:
+    res = file.read()
+    result = chardet.detect(res)
+    out = res.decode(result['encoding']).encode('utf-8')
+    print(out.decode('utf-8'))
 
-with open('test_file.txt', encoding='utf-8') as file:
-    print(file.read())
-    file.close()
 
 
 
