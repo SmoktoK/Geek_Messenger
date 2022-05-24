6b9ede26573c4234f57b598437f6cae5a0d7f7b1
Dima Smok <smokotoff@yandex.ru>
2022-05-24 15:59:13 +0300

    Add server_test

--- Messenger/unit_test/test_client.py
+++ Messenger/unit_test/test_client.py
@@ -1,4 +1,33 @@
-import sys
-# print(sys.path)
-x = sys.path.append('/Messenger/common')
-print(x)
+from Messenger.common.variables import *
+from Messenger.client import create_presence, process_ans
+
+import unittest
+
+
+# Класс с тестами
+class TestClient(unittest.TestCase):
+    #  Проверка соединения
+    def test_def_presence(self):
+        test = create_presence('Guest')
+        test[TIME] = 1
+        self.assertEqual(test, {ACTION: PRESENCE, TIME: 1, USER: {ACCOUNT_NAME: 'Guest'}})
+
+    def test_username(self):
+        username = create_presence()
+        self.assertEqual(username['user'], {ACCOUNT_NAME: 'Guest'})
+        # print(username['user'])
+
+    def test_action(self):
+        action = create_presence()
+        self.assertEqual(action[ACTION], PRESENCE)
+
+    #  Проверка доступности
+    def test_200(self):
+        self.assertEqual(process_ans({RESPONSE: 200}), '200 : OK')
+
+    def test_400(self):
+        self.assertEqual(process_ans({RESPONSE: 400, ERROR: 'Bad Request'}), '400 : Bad Request')
+
+
+if __name__ == '__main__':
+    unittest.main()
